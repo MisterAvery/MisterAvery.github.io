3502a556f4f4e0e6a5ed37d13992039ed5c57482
MisterAvery <58928023+MisterAvery@users.noreply.github.com>
2020-04-03 09:28:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-let nav = document.getElementById('nav');
+let nav = document.querySelector('#nav');
 let links = document.querySelector('.links');
+let hamburger = document.querySelector('.hamburger');
 
 document.addEventListener('click', function(event) {
-  if(window.innerWidth <= 675 && event.target == nav || event.target == links) {
-    if(nav.style.height == '60vh') {
+  if(window.innerWidth <= 675 && event.target == hamburger) {
+    if(nav.style.height == '70vh') {
       nav.style.height = '43px';
       links.style.display = 'none';
     } else {
-      nav.style.height = '60vh';
+      nav.style.height = '70vh';
       links.style.display = 'flex';
     }
   }
@@ -16,7 +17,9 @@ document.addEventListener('click', function(event) {
 window.addEventListener('resize', function() {
   if(window.innerWidth > 675) {
     links.style.display = 'flex';
+    nav.style.height = '50px';
   } else {
     links.style.display = 'none';
+    nav.style.height = '43px';
   }
 })
